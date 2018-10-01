2d8f579d32f025760332b47138649ee527ba7aa1
rey <rey@rey-VirtualBox>
2018-10-01 15:15:55 +0200

    MAJ implémentation Case et énumération de TypeField

--- src/Case.java
+++ src/Case.java
@@ -3,11 +3,44 @@
  */
 
 /**
- * @author rey
+ * @author Rey-Ricord Yoann 
+ *
+ */
+
+/**
+ *
+ *
  *
  */
 
 
 public class Case {
+	
 
+
+	/** Coordinates */
+	
+	int line,column;
+	/** Type of the field  */
+	TypeField nature;
+	
+	/** Access to line*/
+	
+	private int getLine(){
+		return this.line;
+	}
+	
+	/** Acces to column*/
+	
+	private int getColumn(){
+		return this.column;
+	}
+	
+	/** Acces to Type Field*/
+	
+	private TypeField getTypeField(){
+		return this.nature;
+	}
+	
+	
 }
---
+++ src/TypeField.java
@@ -0,0 +1,11 @@
+	
+
+/** enumeration of the different possibilities of the field*/
+	
+public enum TypeField {
+	EMPTY,
+	WATER, 
+	FOREST, 
+	ROCK,
+	HOUSE;
+}
