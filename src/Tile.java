e0d0dd2097161928685b654696a9a0621b3bb3e9
lerbourl <louis.lerbourg@grenoble-inp.org>
2018-10-02 10:08:13 +0200

    Case -> Tile

--- src/Tile.java
+++ src/Tile.java
@@ -1,46 +1,28 @@
 /**
- * 
+ * Represents a tile of the map
  */
 
 /**
  * @author Rey-Ricord Yoann 
- *
- */
-
-/**
- *
- *
- *
  */
 
 
-public class Case {
-	
-
-
+public class Tile {
 	/** Coordinates */
-	
-	int line,column;
+	int line, column;
 	/** Type of the field  */
 	TypeField nature;
 	
 	/** Access to line*/
-	
 	private int getLine(){
 		return this.line;
 	}
-	
 	/** Acces to column*/
-	
 	private int getColumn(){
 		return this.column;
 	}
-	
 	/** Acces to Type Field*/
-	
 	private TypeField getTypeField(){
 		return this.nature;
 	}
-	
-	
 }
