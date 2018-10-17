8c23489a88eb49bfd31aeff7bac795d3790281c3
lerbourl <louis.lerbourg@gmail.com>
2018-10-17 11:30:09 +0200

    ajout nouvelle classe simulator

---
+++ src/poog54/io/Simulator.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package poog54.io;
+
+import gui.Simulable;
+
+/**
+ * @author louis
+ *
+ */
+public class Simulator implements Simulable{
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
